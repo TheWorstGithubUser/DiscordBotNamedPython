58707a88f79ceecb2446c51e4806c1cef5d7290a
TheWorstGithubUser <164255586+TheWorstGithubUser@users.noreply.github.com>
2026-05-27 17:44:15 -0400

    Update viewWebsite.js
    Added newest version of this code that has the required check to see if the list is sorted from newest to oldest

--- commands/utility/viewWebsite.js
+++ commands/utility/viewWebsite.js
@@ -6,47 +6,65 @@ module.exports = {
 		.setName('showsite')
 		.setDescription('Displays the newest articles from newest to oldest'),
 	async execute(interaction) {
-		await interaction.deferReply();
+		await interaction.deferReply(); 
+    //We have to call this method in order to give our bot more time to think.
+    //Default time is 3 seconds, and this gives us 15 minutes
+
 		const articleList = await scrape();
 		// Changing it to a txt file because we have way too many characters
 		const buffer = Buffer.from(articleList, 'utf-8');
         const attachment = new AttachmentBuilder(buffer, { name: 'articles.txt' }); 
-
 		await interaction.editReply({ 
                 files: [attachment] 
             });
-		//await interaction.reply('Displaying the newest articles: ' + articleList);
 	},
 };
+
+//delay function
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function scrape() {
   const browser = await chromium.launch();
 
   const page = await browser.newPage();
 
-  let allTitles = [];
-
   await page.goto(
     'https://news.ycombinator.com/newest',
     { waitUntil: 'domcontentloaded' }
   );
 
+  const allTitles = [];
+  var lastPostDate;
+  var sortedByNewestToOldest = true;
+  
   for (let i = 0; i < 4; i++) {
+    //Grab relevant text from the website
     const ranks = await page.locator('.rank').allInnerTexts();
     const titles = await page.locator('.titleline').allInnerTexts();
+    const age = await page.locator('.age').allInnerTexts();
+    //Grab the UTC time, as the generic age text isn't 100% accurate
+    const exactUTCTimes = await page.locator('.age').evaluateAll(links => 
+      links.map(link => link.getAttribute('title'))
+    );
 
-    ranks.forEach((title, index) => {
-      allTitles.push(ranks[index] + " " + titles[index])
+    ranks.some((rank, index) => {
+      allTitles.push(rank + " " + titles[index] + ", " + age[index]);
+      if(index >= 1 || i >= 1){ //ensures the variable lastPostDate has something in it before we compare
+        var selectedDate = exactUTCTimes[index]; //We have to .split here because exactUTCTimes also grabs
+        if(lastPostDate < selectedDate){
+          sortedByNewestToOldest = false;
+        }
+        lastPostDate = selectedDate;
+      }else{
+        lastPostDate = selectedDate;
+      }
+      return (rank == "100.") //We don't need more than 100. We're pushing Discord's text limit as is anyway
     });
-      
-    
-    //allTitles.push(...ranks);
-    //allTitles.push(...titles);
 
-    const moreLink = page.locator('.morelink');
+    const moreLink = page.locator('.morelink'); //Locate the button to navigate to the next page
 
-    if (await moreLink.count() === 0) {
-      break;
+    if (await moreLink.count() === 0) { //Should never be needed in our case but why not
+      break; 
     }
 
     const next = await moreLink.getAttribute('href');
@@ -55,13 +73,17 @@ async function scrape() {
       `https://news.ycombinator.com/${next}`,
       { waitUntil: 'domcontentloaded' }
     );
+
+    await delay(30000); //hackernews has a crawl-delay of 30 seconds, so this is super slow but necessary
   }
-
-  //console.log(allTitles.join('\n'));
   
-  return String(allTitles.join('\n'))
-
   await browser.close();
+  if(sortedByNewestToOldest)
+    allTitles.push("Articles are all sorted from newest to oldest!")
+  else
+    allTitles.push("The articles are NOT sorted from newest to oldest")
+
+  return allTitles.join('\n');
 }
 
 scrape();
